4f603acc988edfaac7536773595a6c27d85f4822
sugofish <sugofish@gmail.com>
2016-03-17 08:47:47 +0800

    add hubot redis brain

--- scripts/alio.js
+++ scripts/alio.js
@@ -25,4 +25,16 @@ module.exports = function(robot){
   robot.respond(/who talked to u before/, function(response){
     response.send('orrrr....' + who_talk_to_me.toString());
   });
+
+  robot.respond(/count/i, function(response){
+    var count = robot.brain.get('count');
+    count++;
+    robot.brain.set({count:count});
+    response.send('new count = ' + count);
+  });
+
+  robot.respond(/mark my word/i, function(response){
+    robot.brain.set({word:'5566forthewin'});
+    response.send('5566forthewin');
+  });
 }
