78825cb04deae323b3fd8ade64a7a203d107df36
sugofish <sugofish@gmail.com>
2016-03-25 17:29:22 +0800

    add local save

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -1,29 +1,33 @@
+"use strict"
+var Conversation = require('hubot-conversation');
+var serviceKey = 'pomodoro';
+var fs = require('fs');
+var isBrainLoad = false;
+
 module.exports = function (robot) {
-  var serviceKey = 'pomodoro';
+  var switchBoard = new Conversation(robot);
+
+  robot.respond(/backup :tomato:/i, function (response) {
+    checkBrain(robot);
+    backupData(robot);
+    console.log('backup');
+  });
 
   robot.respond(/:tomato:/i, function (response) {
+    checkBrain(robot);
     var userName = response.message.user.name;
-    var tomatoData = robot.brain.get(serviceKey);
-    if (!tomatoData || !tomatoData[userName] || tomatoData[userName].count === 0) {
-      var data = {};
-      data[userName] = {count: 0};
-      robot.brain.set(serviceKey, data);
+    var count = getBrainCount(robot, userName);
+    if (count === 0) {
       response.reply('you don`t hv any :tomato: so far');
-    }else{
-      var tomatoCount = tomatoData[userName].count || 0;
-      response.reply('your :tomato: is ' + tomatoCount + ' now ');
+    } else {
+      response.reply('your :tomato: is ' + count + ' now ');
     }
   });
 
   robot.respond(/did (.*)/i, function (response) {
+    checkBrain(robot);
     var userName = response.message.user.name;
-    var tomatoData = robot.brain.get(serviceKey);
-    if (!tomatoData || !tomatoData[userName]) {
-      var data = {};
-      data[userName] = {count: 0};
-      robot.brain.set(serviceKey, data);
-    }
-    var tomatoCount = tomatoData[userName].count;
+    var tomatoCount = getBrainCount(robot, userName);
     var itemName = response.match[1];
     var newTomato = 0;
     switch (itemName) {
@@ -36,9 +40,72 @@ module.exports = function (robot) {
     if (newTomato === 0) {
       response.send('not in list sorry!');
     } else {
-      tomatoData[userName].count = tomatoCount;
-      robot.brain.set(serviceKey, tomatoData);
+      updateBrain(robot, userName, tomatoCount)
       response.reply('Your :tomato: is ' + tomatoCount + ' now ');
     }
+  });
+
+  robot.respond(/add :tomato:/, function (msg) {
+    var dialog = switchBoard.startDialog(msg);
+    msg.reply('Sure, What is the item name?');
+    dialog.addChoice(/(.*)/i, function (msg2) {
+      var itemName = msg2.match[1];
+      msg2.reply('OK How many :tomato: to ' + itemName);
+      dialog.addChoice(/([0-9]+)/i, function (msg3) {
+        var tomatos = parseInt(msg3.match[1], 10);
+        msg2.reply('So ' + itemName + '= :tomato: X ' + tomatos + '! Right?');
+        dialog.addChoice(/yes/i, function (msg4) {
+          msg4.reply('OK !! will save it ');
+        });
+        dialog.addChoice(/no/i, function (msg5) {
+          msg5.reply('OK !! will leave it! Bye ');
+        });
+      });
+
+    });
+
+
+  });
+}
+
+
+function updateBrain(robot, userName, count) {
+  var sourceData = robot.brain.get(serviceKey);
+  if (sourceData === null) {
+    sourceData = {};
+  }
+  sourceData[userName] = {count: count};
+
+  console.log(JSON.stringify(sourceData));
+  console.log('before:' + userName + ' update \n' + JSON.stringify(robot.brain.get(serviceKey)));
+  robot.brain.set(serviceKey, sourceData);
+  console.log('after:' + userName + ' update \n' + JSON.stringify(robot.brain.get(serviceKey)));
+
+  backupData(robot);
+}
+
+function getBrainCount(robot, userName) {
+  console.log('' + userName + ' GET count \n' + JSON.stringify(robot.brain.get(serviceKey)));
+  var tomatoData = robot.brain.get(serviceKey);
+  if (!tomatoData || !tomatoData[userName]) {
+    return 0;
+  }
+  return tomatoData[userName].count;
+}
+
+function checkBrain(robot) {
+  if (!isBrainLoad) {
+    var content = fs.readFileSync(process.env.BRAIN_BACKUP);
+    console.log(JSON.parse(content));
+    robot.brain.set(serviceKey, JSON.parse(content));
+    isBrainLoad = true;
+  }
+}
+
+function backupData(robot) {
+  var tomatoData = robot.brain.get(serviceKey);
+  fs.writeFile(process.env.BRAIN_BACKUP, JSON.stringify(tomatoData), function (err) {
+    if (err) return console.log(err);
+    console.log('backuped');
   });
 }
